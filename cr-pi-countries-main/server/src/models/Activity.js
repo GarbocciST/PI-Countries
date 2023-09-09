5430744cd6ddb6fe46bc05f5f584c58944c88a14
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-09 14:21:41 -0300

    feat(models): Creado de modelos BD

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,39 @@
+
+const { DataTypes } = require('sequelize');
+
+// Deberás crear dos modelos para tu base de datos. Una será para los países y la otra será para las actividades turísticas (pueden llevar el nombre que tu quieras). La relación entre ambos modelos debe ser de muchos a muchos. A continuación te dejamos las propiedades que debe tener cada modelo. Aquellas marcadas con un asterísco son obligatorias.
+// ID. *
+// Nombre. *
+// Dificultad (número del 1 al 5). *
+// Duración (en horas).
+// Temporada (Verano, Otoño, Invierno o Primavera). *
+
+module.exports = (sequelize) => {
+    sequelize.define('Activity', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        difficulty: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            min: 1,
+            max: 5,
+          }
+        },
+        duration: {
+          type: DataTypes.INTEGER,
+        },
+        season: {
+          type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring'),
+          allowNull: false,
+        },
+    });
+}
--- cr-pi-countries-main/server/src/models/Country.js
+++ cr-pi-countries-main/server/src/models/Country.js
@@ -1,11 +1,50 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+// Deberás crear dos modelos para tu base de datos. Una será para los países y la otra será para las actividades turísticas (pueden llevar el nombre que tu quieras). La relación entre ambos modelos debe ser de muchos a muchos. A continuación te dejamos las propiedades que debe tener cada modelo. Aquellas marcadas con un asterísco son obligatorias.
+// ID (Código de tres letras). *
+// Nombre. *
+// Imagen de la bandera. *
+// Continente. *
+// Capital. *
+// Subregión.
+// Área.
+// Población. *
+
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
   });
