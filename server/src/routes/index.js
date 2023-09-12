4d390c7bc49077d1e37feb7a22eee6593aabd778
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-12 12:12:32 -0300

    fix(server, routes): agregado de rutas correctamente

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,4 +2,26 @@ const { Router } = require("express");
 
 const router = Router();
 
+
+server.get('/countries', (req, res) => {
+    res.send('getCountries')
+})
+  
+server.get('/countries/:id', (req, res) => {
+    res.send('getCountryById')
+})
+  
+server.get('/countries/name', (req, res) => {
+    res.send('getCountryByName')
+})
+  
+server.post('/actvities', (req, res) => {
+    res.send('createActivity')
+})
+  
+server.get('/actvities', (req, res) => {
+    res.send('getActivities')
+})
+  
+
 module.exports = router;
--- server/src/server.js
+++ server/src/server.js
@@ -11,24 +11,4 @@ server.use(cors());
 
 server.use(router);
 
-server.get('/countries', (req, res) => {
-  res.send('getCountries')
-})
-
-server.get('/countries/:id', (req, res) => {
-    res.send('getCountryById')
-})
-
-server.get('/countries/name', (req, res) => {
-    res.send('getCountryByName')
-})
-
-server.post('/actvities', (req, res) => {
-    res.send('createActivity')
-})
-
-server.get('/actvities', (req, res) => {
-    res.send('getActivities')
-})
-
 module.exports = server;
