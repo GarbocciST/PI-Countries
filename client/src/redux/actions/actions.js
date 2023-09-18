c20e068330ca66add1e97db802a92a5fa3afc718
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-18 16:44:21 -0300

    feat(redux): creado de logica y directorio redux

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,47 @@
+import {ADD_ACTIVITY,GET_ACTIVITY, DELETE_ACTIVITY, UPDATE_ACTIVITY, 
+    FILTER_COUNTRIES, ORDER_COUNTRIES, SEARCH_COUNTRY, GET_COUNTRIES,
+    SET_LOADING 
+} from './actions/action_types';
+
+export const getCountries = () => {
+    return async (dispatch) => {
+
+    }
+}
+
+
+export const getActivities = () => {
+    return async (dispatch) => {
+
+    }
+}
+
+export const addActivity = (payload) => {
+    return async (dispatch) => {
+
+    }
+}
+
+export const updateActivity = (payload) => {
+    return async (dispatch) => {
+
+    }
+}
+
+export const deleteActivity = (payload) => {
+    return async (dispatch) => {
+
+    }
+}
+
+export const filter = (payload) => {
+    return async (dispatch) => {
+
+    }
+}
+
+export const order = (payload) => {
+    return async (dispatch) => {
+
+    }
+}
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,78 @@
+import {ADD_ACTIVITY,GET_ACTIVITY, DELETE_ACTIVITY, UPDATE_ACTIVITY, 
+    FILTER_COUNTRIES, ORDER_COUNTRIES, SEARCH_COUNTRY, GET_COUNTRIES, SET_LOADING
+} from './actions/action_types';
+
+
+const initialState = {
+    activities: [],
+    countries: [],
+    allCountries: [],
+    isLoading: false,
+}
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case SET_LOADING: 
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case GET_COUNTRIES:
+            return {
+                ...state,
+                countries: payload,
+                allCountries: payload,
+                isLoading: false,
+            }
+        case GET_ACTIVITY:
+            return {
+                ...state,
+                activities: payload,
+                isLoading: false,
+            }
+        case ADD_ACTIVITY:
+            return {
+                ...state,
+                activities: [...state.activities, payload],
+                isLoading: false,
+            }
+        case UPDATE_ACTIVITY:
+            return {
+                ...state,
+                activities: state.activities.map(activity => activity.id === payload.id ? payload : activity),
+                isLoading: false,
+            }
+        case DELETE_ACTIVITY:
+            return {
+                ...state,
+                activities: state.activities.filter(activity => activity.id !== payload),
+                isLoading: false,
+            }
+        case FILTER_COUNTRIES:
+            const filteredCountries = state.allCountries.filter(country => country.region === payload || country.activities.some(activity => activity.name === payload));
+            return {
+                ...state,
+                allCountries: [...filteredCountries],
+                isLoading: false,
+            }
+        case ORDER_COUNTRIES:
+            const sortedCountries = state.countries.sort((a, b) => {
+                if (a.name > b.name) {
+                    return payload === 'asc' ? 1 : -1;
+                }
+                if (a.name < b.name) {
+                    return payload === 'asc' ? -1 : 1;
+                }
+                return 0;
+            })
+            return {
+                ...state,
+                allCountries: [...sortedCountries],
+                isLoading: false,
+            }
+    }
+
+
+}
+
+export default reducer;
