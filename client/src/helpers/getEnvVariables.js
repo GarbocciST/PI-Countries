ec761f791d1333132b0bed37d141fc14e8a9cd92
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-23 18:52:08 -0300

    fix(getEnvVariables): Eliminado de console.log

--- client/src/helpers/getEnvVariables.js
+++ client/src/helpers/getEnvVariables.js
@@ -1,10 +1,8 @@
-// import { importMeta } from 'vite';
 
 
 export const getEnvVariables = () => {  
 
     const {VITE_API_URL} = import.meta.env;
-    console.log(VITE_API_URL);
     return {
         VITE_API_URL
     }
