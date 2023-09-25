aec1d64bf5489e8c757bae1496e3e5fcfe67cbdd
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-25 17:02:20 -0300

    fix(SearchBar): Agregado de estado para manejar el input

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,23 +1,33 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { searchCountry, setView } from '../../redux/actions';
 
 export const SearchBar = () => {
-
+    const [search, setSearch] = useState('');
     const { view } = useSelector(state => state);
     const country = useSelector(state => state.country);
     const dispatch = useDispatch();
     
+  const handleChange = (e) => {
+      setSearch(e.target.value);
+  }
+
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(searchCountry(e.target[0].value));
-        dispatch(setView('Searched'));
+      e.preventDefault()
+      dispatch(searchCountry(search));
+      dispatch(setView('Searched'));
+      setSearch('');
     }
 
   return (
     <>  
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Search..."/>
+            <input type="text" placeholder="Search..." 
+            onChange={handleChange}
+            value={search}
+            name='search'
+            />
             <button type="submit">Search</button>
         </form>
 
