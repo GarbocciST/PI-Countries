f3001f5ccffc2fa7a2000078f73d7ad843b9a914
Stefano Garbocci <stefgarbocci@gmail.com>
2023-09-27 02:55:52 -0300

    fea(NavBar) : Agregado de disabled en boton View all countries

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { SearchBar } from "../SearchBar/SearchBar";
-import { setView } from "../../redux/actions";
+import { clearCountrySearched, setView } from "../../redux/actions";
 import { Link, useLocation } from "react-router-dom";
 import style from './NavBar.module.css';
 
 export const NavBar = () => {
+  const {countrySearched} = useSelector(state => state);
   const dispatch = useDispatch();
   const { pathname } = useLocation();
 
+
+  const handleView = () => {
+    dispatch(setView('Countries'));
+    dispatch(clearCountrySearched());
+    console.log('render')
+  };
+
   return (
     <div className={style.navBarContainer}>
-      <button onClick={() => dispatch(setView('Countries'))} className={style.navBarButton}>View all countries</button>
+      <button onClick={handleView} className={style.navBarButton} disabled={countrySearched.length === 0} >View all countries</button>
       <SearchBar />
       {pathname === '/home' ? (
         <Link to='/form' >
-          <button className={style.navBarButton}>
+          <button className={style.navBarButton} onClick={ handleView }>
             Go to activities
           </button>
         </Link>
